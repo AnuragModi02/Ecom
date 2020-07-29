683cd16cf5898ee4c0e62f4b16f6e8b07164289d
Anurag <anuragmodi02@yahoo.com>
2020-07-30 04:13:37 +0530

    Intial commit

---
+++ MyEcom/MyEcom/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using MyEcom.Data;
+
+namespace MyEcom.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly StoreContext context;
+
+        public ProductController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            List<Product> products = context.products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+           Product product = context.products.Find(id);
+            return Ok(product);
+        }
+    }
+}
---
+++ MyEcom/MyEcom/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEcom.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> products { get; set; }
+    }
+}
